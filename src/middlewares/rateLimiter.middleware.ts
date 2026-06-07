4bd1644751e36a53efe9f1de4868686bd72adf4d
Harsh Sharma <harshsharmahs.081@gmail.com>
2026-06-07 19:36:35 +0530

    implement rate limiting for API routes; add express-rate-limit and Redis store integration

---
+++ src/middlewares/rateLimiter.middleware.ts
@@ -0,0 +1,23 @@
+import rateLimit from 'express-rate-limit'
+import {RedisStore} from 'rate-limit-redis'
+import { redisClient } from '../../lib/redis.js'
+
+// general api rate limiter 
+export const apiLimiter = rateLimit({
+    windowMs: 15*60*1000,
+    max:100,
+    message:"Too many requests, please try again later",
+    store: new RedisStore({
+        sendCommand:(...args:string[]) => redisClient.sendCommand(args)
+    })
+})
+
+// login/register rate limiter
+export const authLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000,  // 15 minutes
+    max: 10,                    // sirf 10 attempts
+    message: "Too many login attempts, please try again later",
+    store: new RedisStore({
+        sendCommand: (...args: string[]) => redisClient.sendCommand(args)
+    })
+})
--- src/routes/ticket.route.ts
+++ src/routes/ticket.route.ts
@@ -1,24 +1,25 @@
 import { Router } from "express";
 import { createTicket,getUserTickets,getTicketById,updateTicketById, getAnalytics, addMessage, getTicketMessages } from "../controllers/ticket.controller.js";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
+import { apiLimiter } from "../middlewares/rateLimiter.middleware.js";
 
 const router = Router()
 
 // Static routes 
-router.route("/analytics").get(verifyJwt, getAnalytics)
+router.route("/analytics").get(apiLimiter, verifyJwt, getAnalytics)
 // User tickets
-router.route("/my-tickets").get(verifyJwt, getUserTickets)
+router.route("/my-tickets").get(apiLimiter, verifyJwt, getUserTickets)
 
 
 // Dynamic routes
-router.route("/").post(verifyJwt, createTicket)
+router.route("/").post(apiLimiter, verifyJwt, createTicket)
 
-router.route("/:ticketId").get(verifyJwt, getTicketById)
+router.route("/:ticketId").get(apiLimiter, verifyJwt, getTicketById)
 
-router.route("/:ticketId").patch(verifyJwt, updateTicketById)
+router.route("/:ticketId").patch(apiLimiter, verifyJwt, updateTicketById)
 
-router.route("/:ticketId/messages").post(verifyJwt, addMessage)
+router.route("/:ticketId/messages").post(apiLimiter, verifyJwt, addMessage)
 
-router.route("/:ticketId/messages").get(verifyJwt, getTicketMessages)
+router.route("/:ticketId/messages").get(apiLimiter, verifyJwt, getTicketMessages)
 
 export default router
