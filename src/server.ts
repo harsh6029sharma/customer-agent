4e1608eafd45d39701ad69c1977ddc0ff4eff92a
Harsh Sharma <harshsharmahs.081@gmail.com>
2026-07-03 23:09:47 +0530

    feat: add CORS support to enable cross-origin requests

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,18 @@
 import express from 'express'
 import cookieParser from 'cookie-parser'
 import dotenv from 'dotenv'
+import cors from 'cors'
 
 dotenv.config()
 
 const app = express()
+
+app.use(
+  cors({
+    origin: "http://localhost:3002",
+    credentials: true,
+  })
+);
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true, limit:'16kb'}))
